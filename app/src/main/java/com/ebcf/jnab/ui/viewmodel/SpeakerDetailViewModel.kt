ab5738effeb2b0dc47a1575c0ec6630abf00d640
Cristian Camacho <criistian13062000@gmail.com>
2025-05-10 18:18:41 -0300

    feature: ver datos de un expositor

---
+++ app/src/main/java/com/ebcf/jnab/ui/viewmodel/SpeakerDetailViewModel.kt
@@ -0,0 +1,22 @@
+package com.ebcf.jnab.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.ebcf.jnab.data.model.SpeakerModel
+import com.ebcf.jnab.data.repository.SpeakerRepository
+
+class SpeakerDetailViewModel : ViewModel() {
+
+    private val repository = SpeakerRepository()
+
+    private val _speaker = MutableLiveData<SpeakerModel>()
+    val speaker: LiveData<SpeakerModel> = _speaker
+
+    fun loadSpeakerById(id: Int) {
+        val result = repository.getSpeakerById(id)
+        result?.let {
+            _speaker.value = it
+        }
+    }
+}
