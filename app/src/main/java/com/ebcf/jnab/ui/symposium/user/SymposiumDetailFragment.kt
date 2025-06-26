355e9dbf693078dd46c4fd2e54158d9f7b7c2d70
facundopaez-dev <facundo.paez.sd@gmail.com>
2025-06-26 16:52:00 +0000

    chore: renombra DefaultSymposiumRepository como SymposiumRepositoryImpl

--- app/src/main/java/com/ebcf/jnab/ui/symposium/user/SymposiumDetailFragment.kt
+++ app/src/main/java/com/ebcf/jnab/ui/symposium/user/SymposiumDetailFragment.kt
@@ -11,7 +11,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ebcf.jnab.R
-import com.ebcf.jnab.data.repository.DefaultSymposiumRepository
+import com.ebcf.jnab.data.repository.SymposiumRepositoryImpl
 import com.ebcf.jnab.data.source.local.DatabaseProvider
 import com.ebcf.jnab.data.source.remote.FirebaseFirestoreProvider
 import com.ebcf.jnab.data.source.remote.FirestoreSymposiumDataSource
@@ -49,7 +49,7 @@ class SymposiumDetailFragment : Fragment() {
 
         val dao = DatabaseProvider.getDatabase(requireContext()).symposiumDao()
         val remoteDataSource = FirestoreSymposiumDataSource(FirebaseFirestoreProvider.provide())
-        val repository = DefaultSymposiumRepository(dao, remoteDataSource)
+        val repository = SymposiumRepositoryImpl(dao, remoteDataSource)
         val symposiumsListViewModel = SymposiumsListViewModel(repository)
 
         // Crear una instancia del caso de uso
